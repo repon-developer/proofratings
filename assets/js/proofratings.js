628fa5a87ff8498bfb1857aa98e430be22d1e6fc
Repon Hossain <repon.kushtia@gmail.com>
2021-09-08 23:45:04 +0600

    upp

--- assets/js/proofratings.js
+++ assets/js/proofratings.js
@@ -1,15 +1,17 @@
 (function($){
 
     $('.proofratings-floating-badge').on('click', function(){
+        $(this).addClass('opened');
         $('#proofratings-floating-embed').fadeIn(150);
     })
     
     $('#proofratings-floating-embed .proofrating-close').on('click', function(){
+        $('.proofratings-floating-badge').removeClass('opened');
         $('#proofratings-floating-embed').fadeOut(150)
     })
 
     $('#proofratings-floating-embed a.proofratings-widget').on('click', function(){
-        $('html, body').animate({scrollTop: $("#proofratings_widgets").offset().top});        
+        //$('html, body').animate({scrollTop: $("#proofratings_widgets").offset().top});        
     })
 
 })(jQuery)
