f49d1af71a8ce750f330492d3bbdc3527db5b10f
Repon Hossain <repon.kushtia@gmail.com>
2021-12-10 16:43:50 +0600

    uppd

--- src/BadgeDisplay.js
+++ src/BadgeDisplay.js
@@ -3,15 +3,27 @@ const BadgeDisplay = (props) => {
     const badge_display = Object.assign({
         sites_square: false,
         sites_rectangle: false,
-        verall Rating (Rectangle)
+        overall_rectangle: {embed: false, float: false},
+        overall_narrow: {embed: false, float: false},
     }, props.badge_display);
 
-    const update = (name) => {
+    const update_single = (name) => {
         badge_display[name] = !badge_display[name];
-        if ( typeof props.updateSettings === 'function') {
-            props.updateSettings({badge_display})
+
+        if (typeof props.updateSettings === 'function') {
+            props.updateSettings({ badge_display })
         }
     }
+
+    const update_deep = (name, level2) => {
+        badge_display[name][level2] = !badge_display[name][level2];
+
+        if (typeof props.updateSettings === 'function') {
+            props.updateSettings({ badge_display })
+        }
+    }
+
+    const { sites_square, sites_rectangle, overall_rectangle, overall_narrow, overall_cta_banner } = badge_display;
 
 
 
@@ -19,13 +31,13 @@ const BadgeDisplay = (props) => {
         <React.Fragment>
             <table className="form-table">
                 <tbody>
-
+                    <tr>
                         <th scope="row" style={{ verticalAlign: "middle" }}>Sites (Square)</th>
                         <td>
                             <div className="proofratings-image-option">
                                 <img src={`${proofratings.assets_url}images/widget-style1.png`} alt="Proofratings style" />
-                                <label data-tab-button="#settings-badge-square">
-                                    <input onChange={() => update('sites_square')} className="checkbox-switch checkbox-onoff" checked={badge_display.sites_square} type="checkbox" />
+                                <label>
+                                    <input onChange={() => update_single('sites_square')} className="checkbox-switch checkbox-onoff" checked={sites_square} type="checkbox" />
                                     Embed only
                                 </label>
                             </div>
@@ -35,81 +47,67 @@ const BadgeDisplay = (props) => {
                         <th scope="row" style={{ verticalAlign: "middle" }}>Sites (Rectangle)</th>
                         <td>
                             <div className="proofratings-image-option">
-                                <img
-                                    src={`${proofratings.assets_url}images/widget-style2.png`}
-                                    alt="Proofratings style"
-                                />
-                                <label data-tab-button="#settings-badge-rectangle">
-                                    <input className="checkbox-switch checkbox-onoff" onChange={() => update('sites_rectangle')} type="checkbox" checked={badge_display.sites_rectangle} /> Embed only
+                                <img src={`${proofratings.assets_url}images/widget-style2.png`} alt="Proofratings style" />
+                                <label>
+                                    <input className="checkbox-switch checkbox-onoff" onChange={() => update_single('sites_rectangle')} type="checkbox" checked={sites_rectangle} /> Embed only
                                 </label>
                             </div>
                         </td>
                     </tr>
                     <tr>
-                        <th scope="row" style={{ verticalAlign: "middle" }}>
-                            Overall Rating (Rectangle)
-                        </th>
+                        <th scope="row" style={{ verticalAlign: "middle" }}>Overall Rating (Rectangle)</th>
                         <td>
                             <div className="proofratings-image-option">
-                                <img
-                                    src={`${proofratings.assets_url}images/floating-badge-style1.png`}
-                                    alt="Proofratings style"
-                                />
-                                <label data-tab-button="#settings-overall-ratings-rectangle">
-                                    <input
-                                        name="proofratings_display_badge[overall_ratings_rectangle]"
-                                        className="checkbox-switch checkbox-onoff"
-                                        defaultValue="yes"
-                                        type="checkbox"
-                                        defaultChecked="checked"
-                                    />
-                                    Embed and/or float{" "}
+                                <img src={`${proofratings.assets_url}images/floating-badge-style1.png`} alt="Proofratings style" />
+                                <label style={{ marginRight: 30 }}>
+                                    <input className="checkbox-switch checkbox-onoff" type="checkbox" checked={overall_rectangle?.embed} onChange={() => update_deep('overall_rectangle', 'embed')} /> Embed
+                                </label>
+
+                                <label>
+                                    <input className="checkbox-switch checkbox-onoff" type="checkbox" checked={overall_rectangle?.float} onChange={() => update_deep('overall_rectangle', 'float')} /> Float
                                 </label>
                             </div>
                         </td>
                     </tr>
                     <tr>
-                        <th scope="row" style={{ verticalAlign: "middle" }}>
-                            Overall Rating (Narrow)
-                        </th>
+                        <th scope="row" style={{ verticalAlign: "middle" }}>Overall Rating (Narrow)</th>
                         <td>
                             <div className="proofratings-image-option">
-                                <img
-                                    src={`${proofratings.assets_url}images/floating-badge-style2.png`}
-                                    alt="Proofratings style"
-                                />
-                                <label data-tab-button="#settings-overall-ratings-narrow">
+                                <img src={`${proofratings.assets_url}images/floating-badge-style2.png`} alt="Proofratings style" />
+                                <label style={{marginRight: 30}}>
                                     <input
-                                        name="proofratings_display_badge[overall_ratings_narrow]"
-                                        className="checkbox-switch checkbox-onoff"
-                                        defaultValue="yes"
                                         type="checkbox"
-                                        defaultChecked="checked"
+                                        className="checkbox-switch checkbox-onoff"
+                                        onChange={() => update_deep('overall_narrow', 'embed')}
+                                        checked={overall_narrow?.embed}
                                     />
-                                    Embed and/or float{" "}
+                                    Embed
+                                </label>
+
+                                <label>
+                                    <input
+                                        type="checkbox"
+                                        className="checkbox-switch checkbox-onoff"
+                                        onChange={() => update_deep('overall_narrow', 'float')}
+                                        checked={overall_narrow?.float}
+                                    />
+                                    Float
                                 </label>
                             </div>
                         </td>
                     </tr>
                     <tr>
-                        <th scope="row" style={{ verticalAlign: "middle" }}>
-                            Overall Rating CTA Banner
-                        </th>
+                        <th scope="row" style={{ verticalAlign: "middle" }}>Overall Rating CTA Banner</th>
                         <td>
                             <div className="proofratings-image-option">
-                                <img
-                                    src={`${proofratings.assets_url}images/cta-badge.png`}
-                                    alt="Proofratings style"
-                                />
-                                <label data-tab-button="#settings-overall-ratings-cta-banner">
+                                <img src={`${proofratings.assets_url}images/cta-badge.png`} alt="Proofratings style" />
+                                <label>
                                     <input
-                                        name="proofratings_display_badge[overall_ratings_cta_banner]"
-                                        className="checkbox-switch checkbox-onoff"
-                                        defaultValue="yes"
                                         type="checkbox"
-                                        defaultChecked="checked"
-                                    />
-                                    Float only{" "}
+                                        checked={overall_cta_banner}
+                                        className="checkbox-switch checkbox-onoff"
+                                        onChange={() => update_single('overall_cta_banner')}
+                                    /> Float only
                                 </label>
                             </div>
                         </td>
