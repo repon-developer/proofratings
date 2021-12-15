563323b9b4ff6ed336daa490ede3ce7d6229d792
Repon Hossain <repon.kushtia@gmail.com>
2021-12-15 21:24:21 +0600

    upp

--- src/OverallPopup.js
+++ src/OverallPopup.js
@@ -19,6 +19,10 @@ const OverallPopup = () => {
             styles.push('--themeColor:' + state.star_color);
         }
 
+        if ( state?.logo_color ) {
+            styles.push('--logoColor:' + state.logo_color);
+        }
+
         if ( state?.review_text_color ) {
             styles.push('--reviewCountTextColor:' + state.review_text_color);
         }
@@ -39,6 +43,11 @@ const OverallPopup = () => {
     }
 
     const css_style = `.proofratings-popup-widgets-box .proofratings-widget{${get_styles().join(';')}}`;
+
+    let widget_class = 'proofratings-widget proofratings-widget-customized';
+    if ( state?.logo_color ) {
+        widget_class += ' proofratings-widget-logo-color';
+    }
 
     return (
         <React.Fragment>
@@ -64,8 +73,8 @@ const OverallPopup = () => {
                                 <td colSpan={2} style={{paddingLeft: 0}}>
                                     <div className="proofratings-badges-popup">
                                         <div className="proofratings-popup-widgets-box" data-column={3}>
-                                            <div className="proofratings-widget proofratings-widget-google proofratings-widget-customized">
-                                                <div className="review-site-logo">
+                                            <div className={widget_class}>
+                                                <div className="review-site-logo" style={{'WebkitMaskImage': `url(${proofratings.assets_url}/images/google.svg)`}}>
                                                     <img src={`${proofratings.assets_url}/images/google.svg`} alt="Google"/>
                                                 </div>
                                                 <div className="proofratings-reviews" itemProp="reviewRating">
@@ -78,8 +87,8 @@ const OverallPopup = () => {
                                                 <p className="view-reviews">View Reviews</p>
                                             </div>
 
-                                            <div className="proofratings-widget proofratings-widget-trustpilot proofratings-widget-customized">
-                                                <div className="review-site-logo">
+                                            <div className={widget_class}>
+                                                <div className="review-site-logo" style={{WebkitMaskImage: `url(${proofratings.assets_url}/images/trustpilot.svg)`}}>
                                                     <img src={`${proofratings.assets_url}/images/trustpilot.svg`} alt="Trustpilot"/>
                                                 </div>
                                                 <div className="proofratings-reviews" itemProp="reviewRating">
@@ -92,8 +101,8 @@ const OverallPopup = () => {
                                                 <p className="view-reviews">View Reviews</p>
                                             </div>
 
-                                            <div className="proofratings-widget proofratings-widget-wordpress proofratings-widget-customized">
-                                                <div className="review-site-logo">
+                                            <div className={widget_class}>
+                                                <div className="review-site-logo" style={{WebkitMaskImage: `url(${proofratings.assets_url}/images/trustpilot.svg)`}}>
                                                     <img src={`${proofratings.assets_url}/images/wordpress.svg`} alt="Wordpress"/>
                                                 </div>
                                                 <div className="proofratings-reviews" itemProp="reviewRating">
@@ -109,9 +118,15 @@ const OverallPopup = () => {
                                     </div>
                                 </td>
                             </tr>
+
                             <tr>
                                 <th scope="row">Star Color</th>
                                 <td><ColorPicker color={state?.star_color} onUpdate={(star_color) => handle_field({star_color})} /></td>
+                            </tr>
+
+                            <tr>
+                                <th scope="row">Logo Color</th>
+                                <td><ColorPicker color={state?.logo_color} onUpdate={(logo_color) => handle_field({logo_color})} /></td>
                             </tr>
 
                             <tr>
