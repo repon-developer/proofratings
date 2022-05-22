c0de5f8ba7a893f088d780da42fc357935349f47
Repon Hossain <repon.kushtia@gmail.com>
2022-05-22 20:50:20 +0600

    upp

--- src/Settings/Connections.js
+++ src/Settings/Connections.js
@@ -6,7 +6,8 @@ const SiteConnections = () => {
     const [search, setSearch] = useState('')
     const [state, setState] = useState(store.getState())
     const connections = (typeof state?.connections === 'object') ? state.connections : {};
-    
+    const connection_approved = Array.isArray(state?.connection_approved) ? state.connection_approved : [];
+
     useEffect(() => {
         const unsubscribe = store.subscribe(() => setState(store.getState()))
         return () => unsubscribe();
@@ -19,7 +20,7 @@ const SiteConnections = () => {
 
     const handle_connections = (slug, key, value) => {
         const connection = (typeof connections[slug] === 'object') ? connections[slug] : {};
-        connections[slug] = Object.assign(connection, {[key]: value});
+        connections[slug] = Object.assign(connection, { [key]: value });
         store.dispatch({ type: ACTIONS.UPDATE_CONNECTIONS, payload: connections });
     }
 
@@ -33,39 +34,57 @@ const SiteConnections = () => {
     if (typeof proofratings?.review_sites === 'object') {
         Object.entries(proofratings.review_sites).forEach((item) => {
             item[1].slug = item[0];
+            item[1].approved = connection_approved.includes(item[0])
             review_sites.push(item[1])
         })
     }
 
     const get_row = (item) => {
         const slug = item.slug, current_connection = (typeof connections[slug] === 'object') ? connections[slug] : {};
-        const review_site = Object.assign(item, {selected: false, url: ''}, current_connection);
+        const review_site = Object.assign(item, { selected: false, url: '' }, current_connection);
+
+        const pending_items = () => <td className="message-pending-connections" colSpan={4}>We are working on you connection and notify you when complete.</td>
+
+        const default_items = () => {
+            if (item.active === true && item.approved === true) {
+                return (
+                    <>
+                        <td className="bold">55</td>
+                        <td className="bold">4</td>
+                        <td className="click-through-url">
+                            <input type="text" defaultValue={review_site.url} onInput={(e) => handle_connections(slug, 'url', e.target.value)} />
+                            {isUrl(review_site.url) ? <a className="fa-solid fa-up-right-from-square" href={review_site.url} target="_blank"></a> : ''}
+                        </td>
+                    </>
+                )
+            }
+
+            return <td colSpan={4}></td>
+        }
 
         return (
             <React.Fragment>
                 <td><input className="checkbox-switch checkbox-onoff" type="checkbox" defaultChecked={review_site.active} onClick={() => handle_check_connection(slug)} /></td>
                 <td className="review-site-logo"><img src={review_site.logo} alt={review_site.name} /></td>
-                <td className="bold">55</td>
-                <td className="bold">4</td>
-                <td className="click-through-url">
-                    <input type="text" defaultValue={review_site.url} onInput={(e) => handle_connections(slug, 'url', e.target.value )} />
-                    {isUrl(review_site.url) ? <a className="fa-solid fa-up-right-from-square" href={review_site.url} target="_blank"></a> : ''}
-                </td>
+
+                {item.active === true && item.approved === false ? pending_items() : default_items()}
             </React.Fragment>
         )
     }
 
     const review_sites_filtered = review_sites.filter(item => {
-        if ( !search.length ) {
+        if (!search.length) {
             return true;
         }
 
-        if ( typeof item.name === 'undefined' ) {
+        if (typeof item.name === 'undefined') {
             return false;
         }
 
         return item.name.toLowerCase().match(new RegExp(search));
     });
+
+
 
     return (
         <React.Fragment>
