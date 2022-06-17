8a8f45204b87a99dd7a5a4b0715442d5bd7f823e
Repon Hossain <repon.kushtia@gmail.com>
2022-06-17 14:12:20 +0600

    uuu

--- inc/class-proofratings-settings.php
+++ inc/class-proofratings-settings.php
@@ -453,7 +453,9 @@ class Proofratings_Settings {
 				</div>
 			</header>
 
-			<iframe src="https://demo.paymentsplugin.com/manage-subscriptions-inline/" width="100%" height="1000px" ></iframe>
+			
+
+			<!-- <iframe src="https://proofratings.me/customer-panel/" width="100%" height="1000px" ></iframe> -->
 		</div>
 		<?php
 	}
