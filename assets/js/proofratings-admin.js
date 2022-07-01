5c1e35129610c9cccc498f3ec688ff7e454d5b12
Repon Hossain <repon.kushtia@gmail.com>
2022-07-01 12:56:30 +0600

    uppp

--- assets/js/proofratings-admin.js
+++ assets/js/proofratings-admin.js
@@ -24,6 +24,7 @@
     $('.proofratings-customer-card .button-discard').on('click', function (e) {
         e.preventDefault();
         $('.proofratings-customer-card').removeClass('proofratings-customer-card-editing');
+        $('.proofratings-customer-card').find('.button-primary').removeClass('inprogress')
     })
 
     $('.proofratings-customer-card .button-card-form').on('click', function (e) {
@@ -35,12 +36,15 @@
 
     card_container.find('.card-number').mask('0000 0000 0000 0000');
 
-
-
     card_container.find('.card-expiry').mask('00 / 00', { placeholder: "MM / YY" });
 
     card_container.on('submit', function (event) {
         let is_valid = true;
+
+        const submit_button = $(this).closest('.proofratings-customer-card').find('.button-primary')
+        if (submit_button.hasClass('inprogress')) {
+            return false;
+        }
 
         $(this).find('.card-number').validateCreditCard(function (result) {
             is_valid = result.valid
@@ -87,18 +91,19 @@
             return false;
         }
 
-        $.post(proofratings_admin.ajax_url, {action: 'proofratings_update_payment_method', number: $(this).find('.card-number').cleanVal(), expiry: card_expiry.join(''), cvc: cvc }, function (response) {
-            if ( response?.success == true ) {
+        submit_button.addClass('inprogress')
+
+        $.post(proofratings_admin.ajax_url, { action: 'proofratings_update_payment_method', number: $(this).find('.card-number').cleanVal(), expiry: card_expiry.join(''), cvc: cvc }, function (response) {
+            if (response?.success == true) {
                 return window.location.reload();
             }
 
             Swal.fire(response?.error);
+        }).always(() => {
+            submit_button.removeClass('inprogress')
         })
 
         return false;
     })
-
-
-
 
 })(jQuery)
